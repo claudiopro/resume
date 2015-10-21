99f488e4d7c32887a9e2752827aebaddf2686ef9
claudiopro <claudio.procida@gmail.com>
2015-10-22 00:50:57 +0200

    Adds gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -24,12 +24,17 @@
 
 var gulp = require('gulp')
 	, sass = require('gulp-sass')
+	, autoprefixer = require('gulp-autoprefixer')
 
 gulp.task('default', ['sass'])
 
 gulp.task('sass', function () {
 	gulp.src('./src/sass/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+			, cascade: false
+		}))
 		.pipe(gulp.dest('./dist/css'))
 })
 
